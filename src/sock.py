8f5039b9be5b986cc37c2cf665faca6fdf72cb6a
Dominik Boboš <bobos.dominik@gmail.com>
2020-03-02 13:41:45 +0100

    added sigint and comments

--- src/sock.py
+++ src/sock.py
@@ -1,5 +1,6 @@
 import socket 	  		            #for all the server work 
 import sys							#for input arguments
+import signal				 	    #for keyboard interrupt handling
 from urllib.parse import urlparse	#for validating url
 
 ##
@@ -8,30 +9,35 @@ from urllib.parse import urlparse	#for validating url
 #	autor: Boboš Dominik (xbobos00)
 ##
 
+#for keyboard interrupt
+def SignalHandler(sig, frame):
+    print('Server on PORT =', TCP_PORT, 'ended its session!')
+    sys.exit(0)
+
 def ValidIp(address):	#checks for valid IP
 	if (isinstance(address, bytes)):
 		address = address.decode('utf-8')
 	try: 
-		socket.inet_aton(address)
+		socket.inet_aton(address)	#IPv4 check
 		return address
 	except:
 		try: 
-			socket.inet_pton(socket.AF_INET6, address)
+			socket.inet_pton(socket.AF_INET6, address)	#IPv6 check
 			return address
 		except:
 			return False
 
 def ValidURL(address):	#checks for valid URL
-	if (isinstance(address, str)):
+	if (isinstance(address, str)):			#encoding string input
 		address = address.encode('utf-8')
 	url = urlparse(address.decode('utf-8'))
-	url = url[2]			
+	url = url[2]							#second item is the wanted
 	if (url == '' or url == ValidIp(url)):	#case when 'A' but IP was entered
 		return False			#end connection
 	return url 					#return url
 
 def ValidAddress(address):	#checks for valid IP or URL
-	if (isinstance(address, str)):
+	if (isinstance(address, str)):			#encoding string input
 		address = address.encode('utf-8')
 	if (ValidIp(address.decode('utf-8')) == False):
 		url = urlparse(address.decode('utf-8'))
@@ -45,23 +51,25 @@ def ValidAddress(address):	#checks for valid IP or URL
 
 #for parsing and finding A or PTR type 
 def AorPTR(data, bool_str):
-	if (bool_str == False):
+	if (bool_str == False):					#decoding bytes input to string
 		if (isinstance(data, bytes)):
-			print(data)
 			data = data.decode('utf-8')
-	if (data.rfind('type') != -1):	#I have GET
+	if (data.rfind('type') != -1):	#I have GET request
 		if (data.find('A', 0, data.find('HTTP')) != -1):
 			return data.rfind('type'), "A"
 		elif (data.find('PTR',0, data.find('HTTP')) != -1):
 			return data.rfind('type'), "PTR"
-	elif (data.rfind(':') != -1):	#I have POST
-		if (data.find('A',data.rfind(':')) != -1):
+		else:
+			return False, False
+	elif (data.rfind(':') != -1):	#I have POST request
+		if (data.find('A',data.rfind(':')) != -1):	#it could contain spaces
 			return data.rfind(':'), "A"
-		elif (data.find('PTR',data.rfind(':')) != -1):
+		elif (data.find('PTR',data.rfind(':')) != -1): #it could contain spaces
 			return data.rfind(':'), "PTR"
+		else:
+			return False, False
 	else:
 		return False, False
-
 
 
 #arguments parsing
@@ -87,6 +95,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET refers to the add
 s.bind((TCP_IP, TCP_PORT))
 #infinite loop
 while (True):
+	signal.signal(signal.SIGINT, SignalHandler)	#waiting for control + c
 	s.listen(1)	#waits for connection
 	conn, addr = s.accept()
 	print ("Connection from:" , conn)
@@ -130,40 +139,36 @@ while (True):
 			url = ValidURL(url[0][5:])
 			if (url == False):
 				print("IP entered, but hostname was expected")
-				conn.send(error_str.encode('utf-8'))
+				conn.send(error_str.encode('utf-8'))	#400 Bad Request
 				conn.close()
 				continue
 			try:
 				ip = socket.gethostbyaddr(url)
 			except:
 				error_str = http + ' 404 Not Found\r\n\r\n'
-				conn.send(error_str.encode('utf-8'))
+				conn.send(error_str.encode('utf-8'))	#400 Bad Request
 				conn.close()
 				continue
 			http_ver = http + " 200 OK\r\n\r\n"
-			send_str = url + ":A=" + ip[2][0] + "\n"
-			
+			send_str = url + ":A=" + ip[2][0] + "\n"	
 			conn.send(http_ver.encode('utf-8'))
 			conn.send(send_str.encode('utf-8'))
 			conn.close()	
 		elif (len(url) == 2 and req_type == 'PTR'):	#type "PTR"
-			
 			if (ValidIp(url[0][5:].decode('utf-8')) == False):
 				print("Hostname entered, but IP was expected")
-				conn.send(error_str.encode('utf-8'))
+				conn.send(error_str.encode('utf-8'))	#400 Bad Request
 				conn.close()
 				continue
 			try:
 				host = socket.gethostbyaddr(url)
 			except:
 				error_str = http + ' 404 Not Found\r\n\r\n'
-				conn.send(error_str.encode('utf-8'))
+				conn.send(error_str.encode('utf-8')) 
 				conn.close()
 				continue
-
 			http_ver = http + " 200 OK\r\n\r\n"
 			host = url + ":PTR=" + host[0] + "\n"
-
 			conn.send(http_ver.encode('utf-8'))
 			conn.send(host.encode('utf-8'))
 			conn.close()
@@ -193,23 +198,21 @@ while (True):
 		send_str = ''
 		final_msg = ''
 		if(data[7] == ''):				#empty post
-			http_ver = http + " 200 OK\r\n\r\n"
-			conn.send(http_ver.encode('utf-8'))
+			conn.send(error_str.encode('utf-8'))
 			conn.close()
 			continue
 
 		err_check = 0;
 		colon_ind = 0
 		req_type = ''
-
-			
+		
 		for x in range(7,len(data)):	#from index 7 to the rest are URLs/IPs
 			colon_ind, req_type = AorPTR(data[x], True)
 
 			if (req_type == 'A'):
 				url = ValidURL(data[x][:colon_ind])
 				if (url == False):
-					err_check = len(data)
+					err_check = len(data)	#just to specify error message
 					print("IP entered, but hostname was expected")
 					break
 				try:
@@ -222,7 +225,7 @@ while (True):
 			elif (req_type == 'PTR'):
 				url = ValidIp(data[x][:colon_ind])
 				if (url == False):
-					err_check = len(data)
+					err_check = len(data)	#just to specify error message
 					print("Hostname entered, but IP was expected")
 					break
 				try:
@@ -232,13 +235,12 @@ while (True):
 					continue	#not included to final response
 				send_str = url + ":PTR=" + host[0] + "\n"				
 			else:
-				conn.send(error_str.encode('utf-8'))
-				conn.close()
+				err_check = len(data)	#just to specify error message
 				break
 			final_msg += send_str
 
 		if (err_check == len(data)-7 or err_check == len(data)):
-			if (err_check == len(data)):
+			if (err_check == len(data)):		#bad request
 				conn.send(error_str.encode('utf-8'))
 				conn.close()
 			else:
